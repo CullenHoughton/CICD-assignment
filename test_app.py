54b7df0cd7c436827256562d48cc6517d2ab7f6d
CullenHoughton <cullenhoughton@my.unt.edu>
2025-03-26 21:55:29 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,4 +5,3 @@ def test_get_message():
         response = client.get('/api/message')
         assert response.status_code == 200
         assert response.json == {'message': 'Hello from Flask!'}
-#hello worldgiot
